84162aea709b3a671ad998e9b6b279841d690659
Samuel Jo <jo.sunghak@gmail.com>
2021-07-22 14:06:24 -0400

    updated hero text(about me)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,19 +3,25 @@ import React from 'react';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
+const handleClick = () =>{
+    console.log('learn more')
+    window.open('https://www.samueljo.net')
+  }
 
-const Hero = (props) => (
+const Hero = () => (
+
   <>
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
           Welcome To <br />
-          My World.
+          Sam's World.
         </SectionTitle>
         <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+          Javascript developer who enjoys clever & elegant solutions to everyday problems. Specializes in React/Node.
+        {/* The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps. */}
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+        <Button onClick={handleClick}>Learn More</Button>
       </LeftSection>
     </Section>
   </>
