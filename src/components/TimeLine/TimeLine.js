6be90e78ba0326613fb83c913234109c9102e7ac
Samuel Jo <jo.sunghak@gmail.com>
2021-07-22 15:00:55 -0400

    updated personal timeline

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,8 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+        I'm a Economics graduate/self taught JS developer. Passionate for clever solutions that can improve everyone's lives. I'm a self-starter who loves
+        to take things apart & rebuild them. Always been an early adopter of new technologies, and entrepreneurial by nature. Lets chat.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
