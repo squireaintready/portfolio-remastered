63dbdfca922e38dafd9995d04b5ce230676fdc77
Samuel Jo <jo.sunghak@gmail.com>
2021-07-22 15:10:24 -0400

    modified contact details

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -20,12 +20,12 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:314-343-3432">917-292-4594</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
           <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
+            jo.sunghak@gmail.com
           </LinkItem>
         </LinkColumn>
       </LinkList>
