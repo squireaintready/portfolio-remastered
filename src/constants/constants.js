ec444185dffb5b1d55ddc6e3b1b058198f483915
Samuel Jo <jo.sunghak@gmail.com>
2021-07-22 14:35:24 -0400

    updated constants/projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,40 +1,104 @@
 export const projects = [
+  // {
+  //   title: "Google Clone",
+  //   description:
+  //     "Utilized Google custom search JSON API to return personalized search results. Reactjs for the frontend; Condescription API for data layering, material-ui, and firebase for hosting.",
+  //   image: 'images/google.png',
+  //   tags: ['React', 'Javascript'],
+  //   demo: "https://clone-56e6c.web.app/",
+  //   git: "https://github.com/squireaintready/google-clone",
+  //   id:0
+  // },
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 0,
+    title: "Hulu Clone",
+    description:
+      "Used the movie database API (TMDB) to return sorted movie objects. Reactjs for the frontend, functional components with hooks, react router, material UI, flexbox, and deployed on firebase hosting.",
+    image: 'images/hulu.png',
+    tags: ['React', 'Javascript'],
+    demo: "https://hulu-clone-65a1a.web.app/",
+    git: "https://github.com/squireaintready/hulu-clone",
+    id:1
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 1,
+    title: "Covid-19 Global Tracker",
+    description:
+      "Utilized Disease.sh API to retrieve/update live data, i.e. number of new cases, total recovered, and totals deaths. Leaflet API for maps, Chart.js for data visualization, and deployed on firebase hosting.",
+    image: 'images/covidtracker.png',
+    tags: ['React', 'Javascript'],
+    demo: "https://covid-19-tracker-31079.web.app/",
+    git: "https://github.com/squireaintready/covid-tracker",
+    id:2
   },
+  // {
+  //   title: "AirBnb Clone",
+  //   description:
+  //     "Datepicker API to return results based on user's travel/search date parameters. Reactjs for the front end, functional components with hooks, react-router, and flexbox. Deployed on Firebase hosting.",
+  //   image: 'images/airbnb.png',
+  //   tags: ['React', 'Javascript'],
+  //   demo: "https://airbnb-clone-9d9bd.web.app/",
+  //   git: "https://github.com/squireaintready/airbnb-clone",
+  //   id:3
+  // },
+  // {
+  //   title: "Whatsapp Clone",
+  //   description:
+  //     "Leveraged Google cloud firestoreDB for live db, firebase for hosting & google log in authentication. Functional components with hooks, react-router, condescription API, flexbox, and material UI.",
+  //   image: 'images/whatsapp.png',
+  //   tags: ['React', 'Javascript'],
+  //   demo: "https://whatsapp-clone-62615.web.app/",
+  //   git: "https://github.com/squireaintready/whatsapp-clone",
+  //   id:4
+  // },
   {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
+    title: "Spotify Clone",
+    description:
+      "Used Spotify's amazing API for user authentication, getting users playlists, and play a sample. Reactjs for the front end with material-ui, and condescription API for data layering. Deployed on firebase hosting.",
+    image: 'images/spotify.png',
+    tags: ['React', 'Javascript'],
+    demo: "https://spotify-clone-3a930.web.app/",
+    git: "https://github.com/squireaintready/spotify-clone",
+    id:5
   },
+  // {
+  //   title: "Discord Clone",
+  //   description:
+  //     "Google cloud firestoreDB to store/retieve messages live. Firebase for user login/authentication and hosting. Used React.js for the frontend, materialUI, and redux for state mangement.",
+  //   image: 'images/discord.png',
+  //   tags: ['React', 'Javascript'],
+  //   demo: "https://discord-clone-35080.web.app/",
+  //   git: "https://github.com/squireaintready/discord-clone",
+  //   id:6
+  // },
   {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
+    title: "Mizumi Calculator",
+    description:
+      "Developed a tip calculator for a large restaurant(50+ employees) to aid with tips/wage distribution using an in-house algorithm. Built fully with vanilla Javascript and Jquery. This was my first production project.",
+    image: 'images/calculator.png',
+    tags: ['React', 'Javascript'],
+    demo: "https://squireaintready.github.io/advancedTip/",
+    git: "https://github.com/squireaintready/advancedTip",
+    id:7
   },
+  // {
+  //   title: "Slack Clone",
+  //   description:
+  //     "Utilized Google cloud firestore DB for live database. Firebase for user login/authentication and hosting. Frontend React.js, materialUI, flexbox, and condescription API.",
+  //   image: 'images/slack.png',
+  //   tags: ['React', 'Javascript'],
+  //   demo: "https://slack-clone-b635a.web.app/",
+  //   git: "https://github.com/squireaintready/slack-clone",
+  //   id:8
+  // },
+  // {
+  //   title: "Tik-tok Clone",
+  //   description:
+  //     "Frontend React JS: Hooks, Flexbox, Material UI. Firebase database and hosting.",
+  //   image: 'images/.png',
+  // tags: ['React', 'Javascript'],//   
+  // demo: "https://tiktok-clone-f9e09.web.app/",
+  //   git: "https://github.com/squireaintready/tiktok-clone",
+  // id:0
+  // },
 ];
 
 export const TimeLineData = [
